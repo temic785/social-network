fda480710bed83c4512c7ffdec19af6d696b241d
temic785 <teplygin07@mail.ru>
2022-11-08 01:11:07 +0300

    Start project

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        alt="logo"
+        src="https://upload.wikimedia.org/wikipedia/ru/2/28/%D0%A1%D0%B0%D0%BA%D0%B0%D1%80%D1%8C%D1%8F%D1%81%D0%BF%D0%BE%D1%80_%28%D0%BB%D0%BE%D0%B3%D0%BE%29.png"
+      />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <img
+        className="content-pict"
+        alt="content"
+        src="https://img3.goodfon.ru/wallpaper/nbig/1/b1/art-kartinka-skachyat.jpg"
+      />
+      <img
+        alt="cat"
+        src="https://avatars.mds.yandex.net/i?id=2e71a60a70fa89357f90016ab2e9085e-5419733-images-thumbs&n=13"
+      />
+    </div>
+  );
+};
+
+export default Profile;
