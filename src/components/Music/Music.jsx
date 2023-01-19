f00b1967bb0eae217672c097ba022c8d2df8c437
Artyom <teplygin07@mail.ru>
2023-01-19 12:50:50 +0000

    create BrowserRouter

---
+++ src/components/Music/Music.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import Module from "./Music.module.css";
+
+const Music = (props) => {
+  return <div>Music</div>;
+};
+export default Music;
---
+++ src/components/News/News.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import module from "./News.module.css";
+
+const News = (props) =>{
+    return(
+        <div>News</div>
+    )
+};
+export default News;   
